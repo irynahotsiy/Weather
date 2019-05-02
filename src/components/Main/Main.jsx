f20f3fa7ac5fd08beeba4ffe02d193911972603d
Iryna Vovk <irynavovk.s@gmail.com>
2019-05-02 10:28:40 +0300

    first commit

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,111 @@
+import React, { Component } from "react";
+import Load from "../Load/load";
+import { Header, Details, Temp, Image } from "./Style";
+
+class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoaded: false
+    };
+  }
+
+  componentDidMount() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        let lat = position.coords.latitude;
+        let long = position.coords.longitude;
+        let url =
+          "https://fcc-weather-api.glitch.me/api/current?lon=" +
+          long +
+          "&lat=" +
+          lat;
+        console.log(url);
+        fetch(url)
+          .then(response => response.json())
+          .then(
+            result => {
+              this.setState({
+                isLoaded: true,
+                location: result.name + ", " + result.sys.country,
+                temp: Math.round(result.main.temp),
+                sky: result.weather[0].description,
+                img: result.weather[0].icon,
+                letter: "C"
+              });
+            },
+            error => {
+              this.setState({
+                isLoaded: true,
+                error
+              });
+            }
+          );
+      });
+    }
+  }
+
+  handleClickLetter = () => {
+    let temp = this.state.temp;
+    if (this.state.letter === "C") {
+      this.setState({
+        letter: "F",
+        temp: (temp * 9) / 5 + 32
+      });
+    } else {
+      this.setState({
+        letter: "C",
+        temp: ((temp - 32) * 5) / 9
+      });
+    }
+  };
+
+  render() {
+    const { error, isLoaded, img, location, temp, letter, sky } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      debugger;
+      return (
+        <>
+          <Load />
+        </>
+      );
+    } else {
+      return (
+        <>
+          <Header>
+            Free C<img src={img} alt="Weather-icon" />
+            de Camp
+            <br />
+            Weather App
+          </Header>
+          <Details className="text-center">
+            <p>{location}</p>
+            <Temp>
+              <span>{temp > 0 ? "+" + temp + " °" : temp}</span>
+              <span onClick={this.handleClickLetter}>{letter}</span>
+            </Temp>
+            <p>{sky}</p>
+            <Image>
+              <img src={img} alt="Weather-icon" />
+            </Image>
+
+            <footer>
+              Created by
+              <a
+                href="https://ihotsiy.netlify.com"
+                target="blank"
+                id="color"
+              >
+                {" "}
+                Iryna Vovk
+              </a>
+            </footer>
+          </Details>
+        </>
+      );
+    }
+  }
+}
+export default Main;
---
+++ src/components/Main/Style.js
@@ -0,0 +1,237 @@
+import styled from 'styled-components';
+
+
+export const Loading = styled.div `
+font-size: 3rem;
+color: white;
+align-items: center;
+margin-top: 200px;
+
+
+`
+export const Header = styled.header `
+    text-align:center;
+    font-size:70px;
+    color:#ffffff;
+`
+
+export const Details = styled.div `
+    text-align:center;
+    font-size: 1.3rem;
+    font-family:serif;
+    color: #d9f3ef;
+    p {
+        text-transform: capitalize;
+    }
+    footer{
+        font-size: 1.1rem;
+        position: absolute;
+        bottom: 20px;
+        right: 0;
+        left: 0;
+    }
+    a{
+        color:hsl(150, 82%, 51%);
+        text-decoration: none;
+    }
+`
+export const Temp = styled.div `
+    font-size: 1.5rem;
+
+    span:first-child {
+        color: #fcfcfc;
+    }
+    span:last-child {
+        cursor: pointer;
+        color: hsl(150, 82%, 51%);
+    }
+`
+export const Image = styled.div `
+    animation-duration: 3s;
+    animation-iteration-count: infinite;
+    animation-name: clouds;
+    img {
+        width: 80px;
+    }
+
+    @keyframes clouds {
+    0% {
+        -webkit-transform: scale(0.5, 0.5);
+        transform: scale(0.5, 0.5)
+        }
+    50% {
+        -webkit-transform: scale(1.0, 1.0);
+        transform: scale(1.0, 1.0);
+    }
+    100% {
+        -webkit-transform: scale(0.5, 0.5);
+        transform: scale(0.5, 0.5);
+    }
+}
+
+`
+
+/******* Loading ***********/
+export const Container = styled.div `
+    width: 200px;
+    height: 100px;
+    padding-top: 0px;
+    margin: 0 auto;
+`
+export const Ball = styled.div `
+    width: 10px;
+    height: 10px;
+    margin: 10px auto;
+    border-radius: 50px;
+    
+    &:nth-child(1) {
+        background: #ffffff;
+        -webkit-animation: right 1s infinite ease-in-out;
+        -moz-animation: right 1s infinite ease-in-out;
+        animation: right 1s infinite ease-in-out;
+    }
+
+    &:nth-child(2) {
+        background: #ffffff;
+        -webkit-animation: left 1.1s infinite ease-in-out;
+        -moz-animation: left 1.1s infinite ease-in-out;
+        animation: left 1.1s infinite ease-in-out;
+    }
+
+    &:nth-child(3) {
+        background: #ffffff;
+        -webkit-animation: right 1.05s infinite ease-in-out;
+        -moz-animation: right 1.05s infinite ease-in-out;
+        animation: right 1.05s infinite ease-in-out;
+    }
+
+    &:nth-child(4) {
+        background: #ffffff;
+        -webkit-animation: left 1.15s infinite ease-in-out;
+        -moz-animation: left 1.15s infinite ease-in-out;
+        animation: left 1.15s infinite ease-in-out;
+    }
+
+    &:nth-child(5) {
+        background: #ffffff;
+        -webkit-animation: right 1.1s infinite ease-in-out;
+        -moz-animation: right 1.1s infinite ease-in-out;
+        animation: right 1.1s infinite ease-in-out;
+    }
+
+    &:nth-child(6) {
+        background: #ffffff;
+        -webkit-animation: left 1.05s infinite ease-in-out;
+        -moz-animation: left 1.05s infinite ease-in-out;
+        animation: left 1.05s infinite ease-in-out;
+    }
+
+    &:nth-child(7) {
+        background: #ffffff;
+        -webkit-animation: right 1s infinite ease-in-out;
+        -moz-animation: right 1s infinite ease-in-out;
+        animation: right 1s infinite ease-in-out;
+    }
+    &:nth-child(8) {
+        background: #ffffff;
+        -webkit-animation: left 1.05s infinite ease-in-out;
+        -moz-animation: left 1.05s infinite ease-in-out;
+        animation: left 1.05s infinite ease-in-out;
+    }
+
+    &:nth-child(9) {
+        background: #ffffff;
+        -webkit-animation: right 1s infinite ease-in-out;
+        -moz-animation: right 1s infinite ease-in-out;
+        animation: right 1s infinite ease-in-out;
+    }
+    @-webkit-keyframes right {
+        0% {
+            -webkit-transform: translate(-15px);
+        }
+        50% {
+            -webkit-transform: translate(15px);
+        }
+        100% {
+            -webkit-transform: translate(-15px);
+        }
+    }
+
+    @-webkit-keyframes left {
+        0% {
+            -webkit-transform: translate(15px);
+        }
+        50% {
+            -webkit-transform: translate(-15px);
+        }
+        100% {
+            -webkit-transform: translate(15px);
+        }
+    }
+
+    @-moz-keyframes right {
+        0% {
+            -moz-transform: translate(-15px);
+        }
+        50% {
+            -moz-transform: translate(15px);
+        }
+        100% {
+            -moz-transform: translate(-15px);
+        }
+    }
+
+    @-moz-keyframes left {
+        0% {
+            -moz-transform: translate(15px);
+        }
+        50% {
+            -moz-transform: translate(-15px);
+        }
+        100% {
+            -moz-transform: translate(15px);
+        }
+    }
+
+    @keyframes right {
+        0% {
+            transform: translate(-15px);
+        }
+        50% {
+            transform: translate(15px);
+        }
+        100% {
+            transform: translate(-15px);
+        }
+    }
+
+    @keyframes left {
+        0% {
+            transform: translate(15px);
+        }
+        50% {
+            transform: translate(-15px);
+        }
+        100% {
+            transform: translate(15px);
+        }
+    }
+    `
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
